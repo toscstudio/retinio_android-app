695db4aca76b9a1ec956b64d5a7c6b684853a361
roalts <raghav.apoorv@gmail.com>
2016-04-02 21:13:18 +0530

    store adapter

--- app/src/main/java/com/retinio/StoreAdapter.java
+++ app/src/main/java/com/retinio/StoreAdapter.java
@@ -4,6 +4,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -27,18 +29,45 @@ public class StoreAdapter extends RecyclerView.Adapter<StoreAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(StoreAdapter.ViewHolder holder, int position) {
+        holder.storeName.setText(storesList.get(position).getName());
+        holder.storeAddress.setText(storesList.get(position).getAddress());
+        holder.docAvailable.setVisibility(View.GONE);
+        holder.buyAvailable.setVisibility(View.GONE);
+        if(storesList.get(position).isBuyAvailable())
+            holder.buyAvailable.setVisibility(View.VISIBLE);
+        if(storesList.get(position).isDocAvailable())
+            holder.docAvailable.setVisibility(View.VISIBLE);
 
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return storesList.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        TextView storeName;
+        TextView storeAddress;
+        TextView storeDistance;
+        ImageView storeLocation;
+        ImageView storeRating;
+        ImageView storePhoto;
+        ImageView docAvailable;
+        ImageView buyAvailable;
+
         public ViewHolder(View itemView) {
             super(itemView);
+
+            storeName = (TextView) itemView.findViewById(R.id.store_name);
+            storeAddress = (TextView) itemView.findViewById(R.id.store_address);
+            storeDistance = (TextView) itemView.findViewById(R.id.store_distance);
+            storeLocation = (ImageView) itemView.findViewById(R.id.store_location);
+            storeRating = (ImageView) itemView.findViewById(R.id.store_rating);
+            storePhoto = (ImageView) itemView.findViewById(R.id.store_photo);
+            docAvailable = (ImageView) itemView.findViewById(R.id.store_doc_available);
+            buyAvailable = (ImageView) itemView.findViewById(R.id.store_buy_available);
+
         }
     }
 }
