4fb2c74bd508ccc788d7ad7f18cb75df5e22071c
Arnav Gupta <championswimmer@gmail.com>
2016-04-12 05:18:16 +0530

    check this intro bitches

--- app/src/main/java/com/retinio/login/IntroActivity.java
+++ app/src/main/java/com/retinio/login/IntroActivity.java
@@ -2,6 +2,7 @@ package com.retinio.login;
 
 import android.content.Intent;
 import android.graphics.Point;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,6 +14,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Display;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.github.hmallet.realparallaxandroid.RealHorizontalScrollView;
 import com.github.hmallet.realparallaxandroid.RealViewPager;
@@ -25,6 +27,7 @@ import java.util.List;
 public class IntroActivity extends AppCompatActivity {
 
     Button skipButton;
+    AnimationDrawable glassAnim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,10 @@ public class IntroActivity extends AppCompatActivity {
         }
 
         skipButton = (Button) findViewById(R.id.btn_skip);
+        if (findViewById(R.id.intro_glass_animator) != null) {
+            glassAnim = (AnimationDrawable) ((ImageView) findViewById(R.id.intro_glass_animator)).getDrawable();
+        }
+        glassAnim.start();
 
         RealHorizontalScrollView realHorizontalScrollView = (RealHorizontalScrollView) findViewById(R.id.main_horizontal_scrollview);
         final RealViewPager realViewPager = (RealViewPager) findViewById(R.id.main_view_pager);
--- app/src/main/java/com/retinio/login/IntroFragment.java
+++ app/src/main/java/com/retinio/login/IntroFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.retinio.R;
@@ -15,7 +16,6 @@ import com.retinio.R;
  * A simple {@link Fragment} subclass.
  */
 public class IntroFragment extends Fragment {
-
 
     public static IntroFragment newInstance(int position) {
         IntroFragment fragment = new IntroFragment();
@@ -37,19 +37,24 @@ public class IntroFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_intro, container, false);
 
         TextView text = (TextView) rootView.findViewById(R.id.intro_text);
+        ImageView introFace = (ImageView) rootView.findViewById(R.id.intro_fragment_face);
 
         switch (getArguments().getInt("position")) {
             case 0:
                 text.setText("Discover optical stores and eye clinics near you");
+                introFace.setImageResource(R.drawable.intro_male_1);
                 break;
             case 1:
                 text.setText("Book appointments for your next eye checkup");
+                introFace.setImageResource(R.drawable.intro_female_1);
                 break;
             case 2:
                 text.setText("Look for the store with your favourite eyewear brand");
+                introFace.setImageResource(R.drawable.intro_male_2);
                 break;
             case 3:
                 text.setText("Browse through best offers in optical stores near you");
+                introFace.setImageResource(R.drawable.intro_female_2);
                 break;
             default:
                 break;
