560bd1f372dec4e769daecc2f414e3cca1749fe6
Arnav Gupta <championswimmer@gmail.com>
2016-04-15 02:04:15 +0530

    [experimental] use Kotlin for pojos
    it is very easy to write and tiny files

---
+++ app/src/main/java/com/retinio/pojo/Deal.kt
@@ -0,0 +1,12 @@
+package com.retinio.pojo
+
+/**
+ * Created by championswimmer on 15/4/16.
+ */
+
+class Deal {
+    var name:String = "";
+    var desc:String = "";
+    var storeName:String = "";
+
+}
---
+++ app/src/main/java/com/retinio/pojo/Store.kt
@@ -0,0 +1,16 @@
+package com.retinio.pojo
+/**
+ * Created by prempal on 30/3/16.
+ */
+class Store {
+    var name:String = "";
+    var rating:String = "";
+    var address:String = "";
+    var latitude:String = "";
+    var longitude:String = "";
+    var offer:String = "";
+    var imageURL:String = "";
+    var isOfferAvailable:Boolean = false;
+    var isDocAvailable:Boolean = false;
+    var isBuyAvailable:Boolean = false;
+}
