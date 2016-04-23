e703eafc64e86ba1fda0d91b80ab5fe625dd49e5
roalts <raghav.apoorv@gmail.com>
2016-04-23 09:35:34 +0530

    reviews pojo

---
+++ app/src/main/java/com/retinio/pojo/Reviews.java
@@ -0,0 +1,44 @@
+package com.retinio.pojo;
+
+/**
+ * Created by raghav on 23/04/16.
+ */
+public class Reviews {
+    private String name;
+    private String rating;
+    private String review;
+    private String userImage;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+}
